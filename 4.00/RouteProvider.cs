38f4cf46d0f6000fa247b57aecbc3075f4ecc605
Onur Adsay <onuradsay@gmail.com>
2018-07-10 13:19:25 +0300

    Add support for nopCommerce 4.00

---
+++ 4.00/RouteProvider.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Nop.Web.Framework.Mvc.Routing;
+
+namespace Nop.Plugin.DiscountRules.HasProducts
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute("Plugin.DiscountRules.HasProducts.Configure",
+                "Plugins/DiscountRulesHasProducts/Configure",
+                new { controller = "DiscountRulesHasProducts", action = "Configure" });
+
+            routeBuilder.MapRoute("Plugin.DiscountRules.HasProducts.ProductAddPopup",
+                "Plugins/DiscountRulesHasProducts/ProductAddPopup",
+                new { controller = "DiscountRulesHasProducts", action = "ProductAddPopup" });
+
+            routeBuilder.MapRoute("Plugin.DiscountRules.HasProducts.ProductAddPopupList",
+                "Plugins/DiscountRulesHasProducts/ProductAddPopupList",
+                new { controller = "DiscountRulesHasProducts", action = "ProductAddPopupList" });
+
+            routeBuilder.MapRoute("Plugin.DiscountRules.HasProducts.ProductSelectedPopupList",
+                "Plugins/DiscountRulesHasProducts/ProductSelectedPopupList",
+                new { controller = "DiscountRulesHasProducts", action = "ProductSelectedPopupList" });
+
+            routeBuilder.MapRoute("Plugin.DiscountRules.HasProducts.LoadProductFriendlyNames",
+                "Plugins/DiscountRulesHasProducts/LoadProductFriendlyNames",
+                new { controller = "DiscountRulesHasProducts", action = "LoadProductFriendlyNames" });
+        }
+
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
